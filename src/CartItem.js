b1607a60a5d93e9e7365bdf15084c049a4b6a046
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-02-12 21:49:42 +0530

    Update CartItem.js

--- src/CartItem.js
+++ src/CartItem.js
@@ -17,20 +17,29 @@ class CartItem extends React.Component {
             img: ''
         }
     }
+    increaseQty = () => {
+        var q = this.state.qty;
+        this.setState({ qty: q + 1 });
+    }
+    decreaseQty = () => {
+        var q = this.state.qty;
+        if (q > 0)
+            this.setState({ qty: q - 1 });
+    }
     render() {
         return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles} alt = ""/>
+                    <img style={styles} alt="" />
                 </div>
                 <div className="right-block">
                     <div>{this.state.title}</div>
                     <div>Rs. {this.state.price}</div>
                     <div>Quantity: {this.state.qty}</div>
                     <div className="cart-item-actions">
-                        <img alt = "increase" className = "action-icons" src = "https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg"/>
-                        <img alt = "decrease" className = "action-icons" src = "https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"/>
-                        <img alt = "delete" className = "action-icons" src = "https://t4.ftcdn.net/jpg/00/98/26/11/240_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg"/>
+                        <img alt="increase" className="action-icons" onClick={this.increaseQty} src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />
+                        <img alt="decrease" className="action-icons" onClick={this.decreaseQty} src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
+                        <img alt="delete" className="action-icons" src="https://t4.ftcdn.net/jpg/00/98/26/11/240_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg" />
                     </div>
                 </div>
             </div>
