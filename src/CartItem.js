09e7d20f4914f13bcabded5d41f6dc4bb8c9d8a8
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-03-03 23:42:57 +0530

    Update CartItem.js

--- src/CartItem.js
+++ src/CartItem.js
@@ -8,15 +8,7 @@ const styles = {
 }
 
 class CartItem extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            price: 999,
-            title: 'Phone',
-            qty: 1,
-            img: ''
-        }
-    }
+    
     increaseQty = () => {
         var q = this.state.qty;
         this.setState({ qty: q + 1 });
@@ -24,18 +16,19 @@ class CartItem extends React.Component {
     decreaseQty = () => {
         var q = this.state.qty;
         if (q > 0)
-            this.setState({ qty: q - 1 });
+             this.setState({ qty: q - 1 });
     }
     render() {
+        const {price, title, qty} = this.props.product;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles} alt="" />
                 </div>
                 <div className="right-block">
-                    <div>{this.state.title}</div>
-                    <div>Rs. {this.state.price}</div>
-                    <div>Quantity: {this.state.qty}</div>
+                    <div>{title}</div>
+                    <div>Rs. {price}</div>
+                    <div>Quantity: {qty}</div>
                     <div className="cart-item-actions">
                         <img alt="increase" className="action-icons" onClick={this.increaseQty} src="https://t4.ftcdn.net/jpg/01/07/62/07/240_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />
                         <img alt="decrease" className="action-icons" onClick={this.decreaseQty} src="https://t3.ftcdn.net/jpg/03/73/49/86/240_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
