246fa153ea891daad27a62652fd87563b9fd006a
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-03-22 20:13:24 +0530

    Updating quantity of items in firebase

--- src/App.js
+++ src/App.js
@@ -37,15 +37,35 @@ class App extends React.Component {
   increaseQty = (product) => {
     const { products } = this.state;
     const index = products.indexOf(product);
-    products[index].qty += 1;
-    this.setState({ products: products });
+    // products[index].qty += 1; 
+    // this.setState({ products: products });
+    const docRef = this.db.collection('products').doc(products[index].id);
+    docRef
+    .update({
+      qty: products[index].qty + 1,
+    })
+    .then(() => {
+      console.log('Updated successfully');
+    })
+    .catch((err) => {
+      console.log('Error: ' + err);
+    })
   }
   decreaseQty = (product) => {
     const { products } = this.state;
     const index = products.indexOf(product);
     if (products[index].qty > 0) {
-      products[index].qty -= 1;
-      this.setState({ products: products });
+      const docRef = this.db.collection('products').doc(products[index].id);
+      docRef
+      .update({
+        qty: products[index].qty - 1,
+      })
+      .then(() => {
+        console.log('Updated successfully');
+      })
+      .catch((err) => {
+        console.log('Error: ' + err);
+      })
     }
   }
   deleteProduct = (id) => {
@@ -91,7 +111,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
-        <button onClick={this.addProduct} style={{padding: 20, fontSize: 20}}>Add a button</button>
+        {/* <button onClick={this.addProduct} style={{padding: 20, fontSize: 20}}>Add a button</button> */}
         <h1>Cart</h1>
         <Cart
           products={products}
